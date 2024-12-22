760512211fba1cdeffd1303a88f1ec013b5ea911
Kateryna Riabokon <riabokonkaterina44@gmail.com>
2024-12-22 15:39:53 +0200

    Шосте правило. Перейменувала клас та змiннi на бiльш описовi.

--- src/main/java/org/example/ItemCollection.java
+++ src/main/java/org/example/ItemCollection.java
@@ -3,10 +3,10 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Items {
+public class ItemCollection {
     private List<Item> items;
 
-    public Items() {
+    public ItemCollection() {
         this.items = new ArrayList<>();
     }
 
@@ -25,7 +25,7 @@ public class Items {
         }
     }
     private boolean isLegendary(Item item) {
-        String itemName = item.name.getValue();
+        String itemName = item.itemName.getValue();
         return itemName.equals("Sulfuras, Hand of Ragnaros");
     }
     private void updateItemQuality(Item item) {
@@ -37,48 +37,48 @@ public class Items {
         decreaseQuality(item);
     }
     private boolean isSpecialItem(Item item) {
-        String itemName = item.name.getValue();
+        String itemName = item.itemName.getValue();
         return itemName.equals("Aged Brie") || itemName.equals("Backstage passes to a TAFKAL80ETC concert");
     }
     private void increaseQuality(Item item) {
-        int quality = item.quality.getValue();
+        int quality = item.itemQuality.getValue();
         if(quality < 50){
-            item.quality.setValue(item.quality.getValue() + 1);
+            item.itemQuality.setValue(quality + 1);
         }
     }
     private void decreaseQuality(Item item) {
-        int quality = item.quality.getValue();
+        int quality = item.itemQuality.getValue();
         if(quality > 0){
-            item.quality.setValue(item.quality.getValue() - 1);
+            item.itemQuality.setValue(quality - 1);
         }
     }
     private void handleSpecialCases(Item item) {
-        String itemName = item.name.getValue();
+        String itemName = item.itemName.getValue();
         if(!itemName.equals("Backstage passes to a TAFKAL80ETC concert")){
             return;
         }
-        if(item.sellIn.getValue() < 11){
+        if(item.daysRemainingBeforeExpiration.getValue() < 11){
             increaseQuality(item);
         }
-        if(item.sellIn.getValue() < 6){
+        if(item.daysRemainingBeforeExpiration.getValue() < 6){
             increaseQuality(item);
         }
     }
     private void updateSellIn(Item item) {
-        int sellIn = item.sellIn.getValue();
-        item.sellIn.setValue(sellIn - 1);
+        int sellIn = item.daysRemainingBeforeExpiration.getValue();
+        item.daysRemainingBeforeExpiration.setValue(sellIn - 1);
     }
     private void handleExpiredItem(Item item) {
-        if(item.sellIn.getValue() >= 0){
+        if(item.daysRemainingBeforeExpiration.getValue() >= 0){
             return;
         }
-        String itemName = item.name.getValue();
+        String itemName = item.itemName.getValue();
         if(itemName.equals("Aged Brie")){
             increaseQuality(item);
             return;
         }
-        if(item.name.getValue().equals("Backstage passes to a TAFKAL80ETC concert")){
-            item.quality.setValue(0);
+        if(item.itemName.getValue().equals("Backstage passes to a TAFKAL80ETC concert")){
+            item.itemQuality.setValue(0);
             return;
         }
         decreaseQuality(item);
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,25 +3,25 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        Items items = new Items();
-        items.addItem(new Item("Aged Brie", 10, 20));
-        items.addItem(new Item("Elixir of the Mongoose", 5, 7));
-        items.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30));
-        items.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
-        items.addItem(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.addItem(new Item("Sulfuras, Hand of Ragnaros", -1, 80));
-        items.addItem(new Item("Regular Item", 15, 25));
+        ItemCollection itemCollection = new ItemCollection();
+        itemCollection.addItem(new Item("Aged Brie", 10, 20));
+        itemCollection.addItem(new Item("Elixir of the Mongoose", 5, 7));
+        itemCollection.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30));
+        itemCollection.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+        itemCollection.addItem(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        itemCollection.addItem(new Item("Sulfuras, Hand of Ragnaros", -1, 80));
+        itemCollection.addItem(new Item("Regular Item", 15, 25));
 
 
-        GildedRose app = new GildedRose(items);
+        GildedRose app = new GildedRose(itemCollection);
 
         System.out.println("До оновлення:");
-        System.out.println(items);
+        System.out.println(itemCollection);
 
         app.updateQuality();
 
         System.out.println("Після оновлення:");
-        System.out.println(items);
+        System.out.println(itemCollection);
     }
 }
 
