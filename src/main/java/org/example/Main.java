8f25fc60b0d60f2e4b43f46bb720314e543060ff
Kateryna Riabokon <riabokonkaterina44@gmail.com>
2024-12-22 15:01:11 +0200

    Четверте правило. Створила клас Items, що iнкапсулює колекцiю елементiв Item та надає методи для обробки колекцiї.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,31 +3,25 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        Item[] items = new Item[]{
-                new Item("Aged Brie", 10, 20),
-                new Item("Elixir of the Mongoose", 5, 7),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Regular Item", 15, 25)
-        };
+        Items items = new Items();
+        items.addItem(new Item("Aged Brie", 10, 20));
+        items.addItem(new Item("Elixir of the Mongoose", 5, 7));
+        items.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30));
+        items.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+        items.addItem(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        items.addItem(new Item("Sulfuras, Hand of Ragnaros", -1, 80));
+        items.addItem(new Item("Regular Item", 15, 25));
+
 
         GildedRose app = new GildedRose(items);
 
         System.out.println("До оновлення:");
-        printItems(items);
+        System.out.println(items);
 
         app.updateQuality();
 
         System.out.println("Після оновлення:");
-        printItems(items);
-    }
-
-    private static void printItems(Item[] items) {
-        for (Item item : items) {
-            System.out.println(item);
-        }
+        System.out.println(items);
     }
 }
 
