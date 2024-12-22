73dd23c456784024f442f8ffebb33d29b7afb903
Kateryna Riabokon <riabokonkaterina44@gmail.com>
2024-12-22 14:07:54 +0200

    Третє правило. Створила два класи IntegerWrapper та StringWrapper, щоб обгорнути типи int i string.

---
+++ src/main/java/org/example/IntegerWrapper.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class IntegerWrapper {
+    private int value;
+    public IntegerWrapper(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        return Integer.toString(value);
+    }
+}
---
+++ src/main/java/org/example/StringWrapper.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class StringWrapper {
+    private String value;
+    public StringWrapper(String value) {
+        this.value = value;
+    }
+    public String getValue() {
+        return value;
+    }
+    @Override
+    public String toString() {
+        return value;
+    }
+}
